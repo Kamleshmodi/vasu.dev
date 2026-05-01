42d441a47322cdb0632e6f17ba3d7ac7c2759159
Mr. KAMLESH KUMAR GHANCHI <145127161+Kamleshmodi@users.noreply.github.com>
2026-05-01 22:33:31 +0530

    Added new feature

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
+export CARGO_HOME="${CARGO_HOME:-/tmp/cargo}"
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
